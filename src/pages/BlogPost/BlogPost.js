ebdc47332f7a9e5479d0b56d3b95530b4a134e97
SEERRGEE <ieg.sergo@gmail.com>
2023-10-23 12:17:09 -0400

    remove useless code

--- src/pages/BlogPost/BlogPost.js
+++ src/pages/BlogPost/BlogPost.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
-import {format} from 'date-fns'
 import './BlogPost.scss'
 
 const BlogPost = () => {
@@ -11,25 +10,18 @@ const BlogPost = () => {
     const navigate = useNavigate();
 
     const [title, setTitle] = useState('')
-    const [summary, setSummary] = useState('')
     const [blogContent, setBlogContent] = useState('')
     const [image, setImage] = useState('')
     const [dateFinished, setDateFinished] = useState('')
-    const [date, setDate] = useState('')
 
     const projectId = useParams().id
-    console.log(projectId)
 
     useEffect(()=> {
-            window.scrollTo({
-                top: 0
-            });
         
         axios.get(`https://clownfish-app-f6era.ondigitalocean.app/roofingBlog/blogpost/${projectId}`)
         .then((response) => {
             console.log(response.data)
             setTitle(response.data.title)
-            setSummary(response.data.summary)
             setBlogContent(response.data.content)
             setImage(response.data.image.url)
             const foramtDate = new Date(response.data.createdAt);
