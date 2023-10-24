0c74f457c7b5395233969f8a3df7b20896e5caec
SEERRGEE <ieg.sergo@gmail.com>
2023-10-24 16:53:25 -0400

    remove one testimonial

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -63,24 +63,24 @@ const Home = () => {
                         <div className='home__testimonial'>
                             <h3 className='home__customer'>Anna Oscar</h3>
                             <p className='home__description'>Collaborating with Optimum Roofing Solutions has been life-changing.<br/><br/> Their design prowess and attentive service truly stand out.<br/><br/> For extraordinary home transformations, Optimum Roofing Solutions is the ultimate choice!</p>
-                            <h4 className='home__testimonial-date'>July 2022</h4>
+                            <h4 className='home__testimonial-date'>July 2023</h4>
                         </div>
                         <div className='home__testimonial'>
                             <h3 className='home__customer'>Fredrick Slapa</h3>
                             <p className='home__description'>Optimum Roofing Solutions has left a lasting impression on me.<br/><br/> Their dedication to precision and their innovative approach to roofing is truly impressive.<br/><br/> I've had the opportunity to collaborate with various companies, but unique blend of creativity and craftsmanship sets them apart. </p>
-                            <h4 className='home__testimonial-date'>December 2021</h4>
+                            <h4 className='home__testimonial-date'>August 2023</h4>
                         </div>
                         <div className='home__testimonial'>
                             <h3 className='home__customer'>Dan Hassan</h3>
                             <p className='home__description'>Partnering with Optimum Roofing Solutions has transformed my home.<br/><br/> Their expertise in roofing, along with their superb customer service, made the entire process a pleasure.<br/><br/> For exceptional home upgrades, Optimum Roofing Solutions is the name to trust!</p>
-                            <h4 className='home__testimonial-date'>June 2022</h4>
+                            <h4 className='home__testimonial-date'>June 2023</h4>
                         </div>
-                        <div className='home__testimonial'>
+                        {/* <div className='home__testimonial'>
                             <h3 className='home__customer'>Rodrigo Ferrierra De Castro</h3>
                             <p className='home__description'>Teaming up with Optimum Roofing Solutions is a game-changer.
                                 Their roofing design know-how, plus top-tier service, made it a delightful journey.<br/><br/>For unmatched home enhancements, trust Optimum Roofing Solutions!</p>
                             <h4 className='home__testimonial-date'>October 2022</h4>
-                        </div>
+                        </div> */}
                     </div>
                 </section>
                 {/* <section className='home__section'>
